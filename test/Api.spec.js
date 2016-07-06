decb7e9a751a7849dac3fb190aaf008f3739eb13
MarcosBessega <mvbessega8@gmail.com>
2016-07-06 13:58:04 -0300

    travis Passing

--- test/Api.spec.js
+++ test/Api.spec.js
@@ -130,7 +130,7 @@ describe('API Service', () => {
 
 		describe("Memoizes", () => {
 
-			it('Should Memoize getGantt', (done) => {
+			it('Should Memoize getGantt', () => {
 				$httpBackend.expectPOST(api.baseUrl + 'planejamento/getGantt').respond('Gantt Data');
 
 				api.getGantt(1, 2, 3)
@@ -138,53 +138,84 @@ describe('API Service', () => {
 						expect(data.data).toEqual('Gantt Data')
 					})
 				$httpBackend.flush()
-				.then( () => {
-					api.getGantt(1, 2, 3)
-						.then(data => {
-							expect(data.data).toEqual('Gantt Data')
-							done()
-						})
-				})
+				api.getGantt(1, 2, 3)
+					.then(data => {
+						expect(data.data).toEqual('Gantt Data')
+					})
 			})
 
-			// it('Should Memoize cronos', (done) => {
-			// 	$httpBackend.expectPOST(api.baseUrl + 'planejamento/getCrono').respond('Crono Data');
-			//
-			// 	api.getCronogramas(1)
-			// 		.then(data => {
-			// 			expect(data.data).toEqual('Crono Data')
-			// 		})
-			// 	$httpBackend.flush();
-			// 	api.getCronogramas(1)
-			// 		.then(data => {
-			// 			expect(data.data).toEqual('Crono Data')
-			// 			done()
-			// 		})
-			// })
-			//
-			// it('Should Memoize Etapas', (done) => {
-			// 	$httpBackend.expectPOST(api.baseUrl + 'planejamento/getEtapas').respond('Etapa Data');
-			//
-			// 	api.getEtapas(1)
-			// 		.then(data => {
-			// 			expect(data.data).toEqual('Etapa Data')
-			// 		})
-			// 	$httpBackend.flush();
-			// 	api.getEtapas(1)
-			// 		.then(data => {
-			// 			console.log(data);
-			// 			expect(data.data).toEqual('Etapa Data')
-			// 			done()
-			// 		})
-			// })
-			//
-			// it('Should Memoize Avanco', (done) => {
-			//
-			// })
-			//
-			// it('Should Memoize Entrega', (done) => {
-			//
-			// })
+			it('Should Memoize cronos', () => {
+				$httpBackend.expectPOST(api.baseUrl + 'planejamento/getCrono').respond('Crono Data');
+
+				api.getCronogramas(1)
+					.then(data => {
+						expect(data.data).toEqual('Crono Data')
+					})
+				$httpBackend.flush();
+				api.getCronogramas(1)
+					.then(data => {
+						expect(data.data).toEqual('Crono Data')
+					})
+			})
+
+			it('Should Memoize Etapas', () => {
+				$httpBackend.expectPOST(api.baseUrl + 'planejamento/getEtapas').respond('Etapa Data');
+
+				api.getEtapas(1)
+					.then(data => {
+						expect(data.data).toEqual('Etapa Data')
+					})
+				$httpBackend.flush();
+				api.getEtapas(1)
+					.then(data => {
+						console.log(data);
+						expect(data.data).toEqual('Etapa Data')
+					})
+			})
+
+			it('Should Memoize Avanco', () => {
+				$httpBackend.expectPOST(api.baseUrl + 'planejamento/getAvanco').respond('Avanco Data');
+
+				api.getAvanco(1)
+					.then(data => {
+						expect(data.data).toEqual('Avanco Data')
+					})
+				$httpBackend.flush();
+				api.getAvanco(1)
+					.then(data => {
+						expect(data.data).toEqual('Avanco Data')
+					})
+			})
+
+			it('Should Memoize Entrega', () => {
+				$httpBackend.expectPOST(api.baseUrl + 'planejamento/getEntrega').respond('Entrega Data');
+
+				api.getEntrega(1)
+					.then(data => {
+						expect(data.data).toEqual('Entrega Data')
+					})
+				$httpBackend.flush();
+				api.getEntrega(1)
+					.then(data => {
+						expect(data.data).toEqual('Entrega Data')
+					})
+			})
+
+			it('Should NOT Memoize Obras', () => {
+				$httpBackend.expectPOST(api.baseUrl + 'planejamento/getObras').respond('Obras Data');
+
+				api.getObras(1)
+					.then(data => {
+						expect(data.data).toEqual('Obras Data')
+					})
+				$httpBackend.flush();
+				$httpBackend.expectPOST(api.baseUrl + 'planejamento/getObras').respond('Obras Memo');
+				api.getObras(1)
+					.then(data => {
+						expect(data.data).not.toEqual('Obras Data')
+					})
+				$httpBackend.flush();
+			})
 
 		})
 
